3a87f2ea09d0840318434169084e5aaff5c25285
kikiru328 <kikiru328@gmail.com>
2025-02-23 13:59:56 +0900

    Connect database, mysql

---
+++ todos/src/database/connection.py
@@ -0,0 +1,10 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+# register
+DATABASE_URL = "mysql+pymysql://root:todos@127.0.0.1:3306/todos"
+
+# for connection
+engine = create_engine(DATABASE_URL, echo=True) # re-print sql query
+SessionFactory = sessionmaker(autocommit=False, autoflush=False,bind=engine)
+
