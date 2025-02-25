6f0ddb91023e3057c6e4a130300cc9e4cd515891
kikiru328 <kikiru328@gmail.com>
2025-02-25 15:15:22 +0900

    mocking test

--- todos/src/tests/test_main.py
+++ todos/src/tests/test_main.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient # for test
 
+from database.orm import ToDo
 from main import app
 
 client = TestClient(app=app) # sending API
@@ -9,15 +10,19 @@ def test_health_check():
     assert response.status_code == 200
     assert response.json() == {"ping": "pong"}
 
-def test_get_todos():
+def test_get_todos(mocker):
+    # mocking before functions work
+    mocker.patch("main.get_todos", return_value=[
+        ToDo(id=1, contents="FastAPI Section 0", is_done=True),
+        ToDo(id=2, contents="FastAPI Section 1", is_done=False),
+    ])
     # order=ASC
     response = client.get("/todos")
     assert response.status_code == 200
     assert response.json() == {
         "todos": [
             {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
-            {"id": 2, "contents": "FastAPI Section 1", "is_done": True},
-            {"id": 3, "contents": "FastAPI Section 2", "is_done": False},
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
         ]
     }
 
@@ -26,8 +31,7 @@ def test_get_todos():
     assert response.status_code == 200
     assert response.json() == {
         "todos": [
-            {"id": 3, "contents": "FastAPI Section 2", "is_done": False},
-            {"id": 2, "contents": "FastAPI Section 1", "is_done": True},
+            {"id": 2, "contents": "FastAPI Section 1", "is_done": False},
             {"id": 1, "contents": "FastAPI Section 0", "is_done": True},
         ]
     }
