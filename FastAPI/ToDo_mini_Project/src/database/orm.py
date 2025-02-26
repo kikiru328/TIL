83fbb24e43b2a2c3b471fc3b2dabc26c9a2dc0c6
kikiru328 <kikiru328@gmail.com>
2025-02-26 12:51:25 +0900

    Setting ORM base

---
+++ FastAPI/ToDo_mini_Project/src/database/orm.py
@@ -0,0 +1,15 @@
+from sqlalchemy.orm import declarative_base
+from sqlalchemy import Boolean, Column, Integer, String
+
+Base = declarative_base() # default format
+
+class ToDo(Base):
+    __tablename__ = "todo"
+
+    id = Column(Integer, primary_key=True, index=True)
+    contents = Column(String, nullable=False)
+    is_done = Column(Boolean, nullable=False)
+
+    # repr: re-print
+    def __repr__(self):
+        return f"ToDo(id= {self.id}, contents= {self.contents}, is_done= {self.is_done}"
