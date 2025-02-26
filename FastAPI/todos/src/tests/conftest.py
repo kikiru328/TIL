0d97f11a7aed6dbb18e955eaaae04b617f7a62ce
kikiru328 <kikiru328@gmail.com>
2025-02-26 11:10:51 +0900

    Reset Whole Repository

--- FastAPI/todos/src/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-
-from fastapi.testclient import TestClient
-from main import app
-
-# fixture
-@pytest.fixture
-def client(): #this will be fix in test code
-    return TestClient(app=app)
