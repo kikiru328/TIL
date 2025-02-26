9bd9c851ec9e320cba5a0bd6f847c2958e69c51a
kikiru328 <kikiru328@gmail.com>
2025-02-26 13:09:17 +0900

    Refactoring: Repository Design

--- FastAPI/ToDo_mini_Project/src/schema/response.py
+++ FastAPI/ToDo_mini_Project/src/schema/response.py
@@ -6,6 +6,8 @@ class ToDoSchema(BaseModel):
     id: int
     contents: str
     is_done: bool
+    class Config:
+        orm_mode = True
 
 class ToDoListSchema(BaseModel):
     todos: List[ToDoSchema]
