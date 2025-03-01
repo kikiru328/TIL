4fbe20e072c2ab7209ad99ed32648a449cdf1c00
kikiru328 <kikiru328@gmail.com>
2025-03-01 11:02:33 +0900

    Practice Question: Palindrome Linked List

--- Algorithm/QuestionNSolve/Palindrome Linked List.py
+++ Algorithm/QuestionNSolve/Palindrome Linked List.py
@@ -9,7 +9,36 @@ print: True
 insert: [1, 2]
 print: False
 """
-
-from structures import LinkedList
+from structures.LinkedList import LinkedList
 
 linked_list = LinkedList()
+
+l1 = LinkedList()
+for num in [1, 2, 2, 1]: # 1 -> 2 -> 2 -> 1
+    l1.append(num)
+
+l2 = LinkedList()
+for num in [1, 2]: # 1 -> 2
+    l2.append(num)
+
+def is_palindrome(ln) -> bool:
+    arr = []
+    head = ln.head
+
+    if not head:
+        return True
+
+    node = head
+    while node:
+        arr.append(node.value)
+        node = node.next
+
+    while len(arr) > 1:
+        first = arr.pop(0)
+        last =arr.pop()
+        if first != last:
+            return False
+    return True
+
+assert is_palindrome(l1)
+assert not is_palindrome(l2)
