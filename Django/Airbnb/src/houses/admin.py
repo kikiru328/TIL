4e4a43967b83e2bd9deab51b991e6c9f4ab78a21
kikiru328 <kikiru328@gmail.com>
2025-03-05 14:14:45 +0900

    edit admin panel for house model

--- Django/Airbnb/src/houses/admin.py
+++ Django/Airbnb/src/houses/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import House
-
 # Register your models here.
+
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    pass
+    list_display: List[str] = [
+        "name",
+        "price_per_night",
+        "pets_allowed"
+        "address",
+    ]
