f56414df3ec09e1efe7c93e15bb3aa9190776cf1
kikiru328 <kikiru328@gmail.com>
2025-03-06 22:32:48 +0900

    regenerate house model

--- Django/Airbnb/src/houses/admin.py
+++ Django/Airbnb/src/houses/admin.py
@@ -1,21 +1,12 @@
 from typing import List, Tuple
 
 from django.contrib import admin
-from .models import House
+from houses.models import House
 # Register your models here.
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
 
-    fields: Tuple[str] = (
-        "name",
-        "address",
-        (
-            "price_per_night",
-            "pets_allowed",
-        )
-
-    )
     list_display: Tuple[str] = (
         "name",
         "price_per_night",
@@ -26,13 +17,4 @@ class HouseAdmin(admin.ModelAdmin):
     list_filter: Tuple[str] = (
         "price_per_night",
         "pets_allowed",
-    )
-
-    search_fields: Tuple[str] = (
-        "address__startswith",
-    )
-
-    list_display_links: Tuple[str] = (
-        "name",
-        "address",
     )
