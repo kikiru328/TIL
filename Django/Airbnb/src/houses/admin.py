f0b877741063b4338f5067a9e1a74a291e60fc8e
kikiru328 <kikiru328@gmail.com>
2025-03-06 13:21:22 +0900

    customizing admin panel

--- Django/Airbnb/src/houses/admin.py
+++ Django/Airbnb/src/houses/admin.py
@@ -1,12 +1,23 @@
+from typing import List, Tuple
+
 from django.contrib import admin
 from .models import House
 # Register your models here.
 
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    list_display: List[str] = [
+    list_display: Tuple[str] = (
         "name",
         "price_per_night",
-        "pets_allowed"
+        "pets_allowed",
         "address",
-    ]
+    )
+
+    list_filter: Tuple[str] = (
+        "price_per_night",
+        "pets_allowed",
+    )
+
+    search_fields: Tuple[str] = (
+        "address__startswith",
+    )
