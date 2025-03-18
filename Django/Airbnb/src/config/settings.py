4db0091c724fa726c3d08bce2cd7106461c26096
kikiru328 <kikiru328@gmail.com>
2025-03-18 14:24:48 +0900

    user admin

--- Django/Airbnb/src/config/settings.py
+++ Django/Airbnb/src/config/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 # Application definition
 CUSTOM_APPS = [
     "users.apps.UsersConfig",
+    "rooms.apps.RoomsConfig",
     ]
 
 SYSTEM_APPS = [
@@ -109,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ko-kr'
 
 TIME_ZONE = 'Asia/Seoul'
 
