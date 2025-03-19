a191465746ab5aa094d98d8b57143b9ee24df510
kikiru328 <kikiru328@gmail.com>
2025-03-19 23:51:55 +0900

    django template

--- Django/Airbnb/src/rooms/urls.py
+++ Django/Airbnb/src/rooms/urls.py
@@ -4,6 +4,7 @@ from rooms import views
 # <{parameter}:{name parameter}>
 # <int:room_id>/<str:room_name> ==> {int}/{str} ...
 
+
 urlpatterns = [
     path("", views.see_all_rooms), # from rooms/
     path("<int:room_id>", views.see_one_room),
--- Django/Airbnb/src/rooms/views.py
+++ Django/Airbnb/src/rooms/views.py
@@ -1,9 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from rooms.models import Room
+
+
 # Create your views here.
 def see_all_rooms(request):
-    return HttpResponse("see all rooms")
+    rooms = Room.objects.all()
+    return render(
+        request,
+        "all_rooms.html",
+        {
+            "title": "Hello! This title comes from Django!",
+            "rooms": rooms
+        }, # render with html
+    ) # request, template
 
 def see_one_room(request, room_id): # {url parameter}
     return HttpResponse(f"see room with id: {room_id}")
