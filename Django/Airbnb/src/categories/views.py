520531b998385abd362be4bc7b19071b182059f3
kikiru328 <kikiru328@gmail.com>
2025-03-20 10:16:35 +0900

    make api using django restframworks

--- Django/Airbnb/src/categories/views.py
+++ Django/Airbnb/src/categories/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from categories.models import Category
+
 
 # Create your views here.
+@api_view()
+def categories(request):
+    all_categories = Category.objects.all() # queryset
+    return Response(
+        {
+            "ok": True,
+            "categories": all_categories
+        }
+    )
