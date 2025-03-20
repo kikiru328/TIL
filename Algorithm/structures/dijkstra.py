401965e4cbd2f932a6b7596b0fe5802295e51146
kikiru328 <kikiru328@gmail.com>
2025-03-20 21:23:50 +0900

    dijkstra

---
+++ Algorithm/structures/dijkstra.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+
+def dijkstra(graph, start):
+    N = len(graph)
+    INF = 1e9
+    dist = [INF] * N
+
+    q = []
+    # 튜플일 경우 0번째 요소 기준으로 최소 힙 구조.
+    # 첫 번째 방문 누적 비용은 0이다.
+    heapq.heappush(q, (0, start))
+    dist[start] = 0
+
+    while q:
+        # 누적 비용이 가장 작은 녀석을 꺼낸다.
+        acc, cur = heapq.heappop(q)
+
+        # 이미 답이 될 가망이 없다.
+        if dist[cur] < acc:
+            continue
+
+        # 인접 노드를 차례대로 살펴보며 거리를 업데이트한다.
+        for adj, d in graph[cur]:
+            cost = acc + d
+            if cost < dist[adj]:
+                dist[adj] = cost
+                heapq.heappush(q, (cost, adj))
+
+    return dist
+
+with open('dijkstra_testcase.txt') as f:
+    sys.stdin = f
+    input = sys.stdin.readline
+    n, m = map(int, input().split())
+    start = int(input())
+    graph = [[] for _ in range(n + 1)]
+
+    for _ in range(m):
+        a, b, c = map(int, input().split())
+        graph[a].append((b, c))
+
+assert dijkstra(graph, start) == [1000000000, 0, 8, 9, 5, 7]
