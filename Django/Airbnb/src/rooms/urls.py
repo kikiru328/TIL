985229b535890d5570ea84f39a9e921eaf49f69e
kikiru328 <kikiru328@gmail.com>
2025-03-21 12:15:45 +0900

    pagination: room amenities

--- Django/Airbnb/src/rooms/urls.py
+++ Django/Airbnb/src/rooms/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from rooms.views import Rooms, Amenities, AmenityDetail, RoomDetail, RoomReviews
+from rooms.views import Rooms, Amenities, AmenityDetail, RoomDetail, RoomReviews, RoomAmenities
 
 # <{parameter}:{name parameter}>
 # <int:room_id>/<str:room_name> ==> {int}/{str} ...
@@ -10,6 +10,7 @@ urlpatterns = [
     path("", Rooms.as_view()),
     path("<int:pk>", RoomDetail.as_view()),
     path("<int:pk>/reviews", RoomReviews.as_view()),
+    path("<int:pk>/amenities",RoomAmenities.as_view()),
     path("amenities/", Amenities.as_view()),
     path("amenities/<int:pk>", AmenityDetail.as_view()),
 ]
--- Django/Airbnb/src/rooms/views.py
+++ Django/Airbnb/src/rooms/views.py
@@ -207,7 +207,26 @@ class RoomReviews(APIView):
         )
         return Response(serializer.data)
 
+class RoomAmenities(APIView):
 
+    def get_object(self, pk):
+        try:
+            return Room.objects.get(pk=pk)
+        except Room.DoesNotExist:
+            raise NotFound
 
-
-
+    def get(self, request, pk):
+        try:
+            page = request.query_params.get("page", 1)
+            page = int(page)
+        except ValueError:
+            page = 1
+        page_size = 2 # how many items will display
+        start = (page -1 ) * page_size
+        end = start + page_size
+        room = self.get_object(pk=pk)
+        serializer = AmenitySerializer(
+            room.amenities.all()[start:end], # limit:offset
+            many=True,
+        )
+        return Response(serializer.data)
