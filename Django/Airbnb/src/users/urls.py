942d27fa0a21d417f31798d072d957a534d122a2
kikiru328 <kikiru328@gmail.com>
2025-04-04 12:06:11 +0900

    feat(user): password & login/out

--- Django/Airbnb/src/users/urls.py
+++ Django/Airbnb/src/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from users.views import Me, Users
+from users.views import Me, Users, PublicUser, ChangePassword, LogIn, LogOut
 
 urlpatterns= [
     path("", Users.as_view()),
     path("me/", Me.as_view()),
+    path("@<str:username>", PublicUser.as_view()),
+    path("change-password", ChangePassword.as_view()),
+    path("log-in", LogIn.as_view()),
+    path("log-out", LogOut.as_view())
 ]
