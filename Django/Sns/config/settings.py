d55e98a224f9bec284675a0ba6479843fcbece5f
kikiru328 <kikiru328@gmail.com>
2025-04-08 14:16:09 +0900

    chore: reset setting for dir templates activate

--- Django/Sns/config/settings.py
+++ Django/Sns/config/settings.py
@@ -76,9 +76,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            BASE_DIR / "posts/templates"
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
