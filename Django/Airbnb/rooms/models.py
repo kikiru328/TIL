8b19199b91138fa422d9ae3b894cfa66aae45009
kikiru328 <kikiru328@gmail.com>
2025-04-11 16:16:26 +0900

    feat(rooms): Many to Many 6.2

--- Django/Airbnb/rooms/models.py
+++ Django/Airbnb/rooms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from common.models import CommonModel
 
-
-class Room(models.Model):
+class Room(CommonModel):
 
     """Room Model Definition"""
 
@@ -36,9 +36,11 @@ class Room(models.Model):
         "users.User",
         on_delete=models.CASCADE,
     )
+    amenities = models.ManyToManyField(
+        "rooms.Amenity",
+    )
 
-
-class Amenity(models.Model):
+class Amenity(CommonModel):
 
     """Amenity Definiton"""
 
