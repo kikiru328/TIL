19d98ae5b926dd3899a15444c7920f4ac61de0e3
kikiru328 <kikiru328@gmail.com>
2025-04-11 16:28:04 +0900

    feat(rooms): Rooms Admin 6.4

--- Django/Airbnb/rooms/models.py
+++ Django/Airbnb/rooms/models.py
@@ -10,6 +10,10 @@ class Room(CommonModel):
         PRIVATE_ROOM = ("private_room", "Private Room")
         SHARED_ROOM = "shared_room", "Shared Room"
 
+    name = models.CharField(
+        max_length=180,
+        default="",
+    )
     country = models.CharField(
         max_length=50,
         default="한국",
@@ -40,6 +44,9 @@ class Room(CommonModel):
         "rooms.Amenity",
     )
 
+    def __str__(self) -> str:
+        return self.name
+
 class Amenity(CommonModel):
 
     """Amenity Definiton"""
@@ -52,3 +59,9 @@ class Amenity(CommonModel):
         null=True,
         blank=True,
     )
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Amenities"
