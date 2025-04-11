daafc5fdff4dd434ef6c20e8de17e4ca806e2ff5
kikiru328 <kikiru328@gmail.com>
2025-04-11 17:15:07 +0900

    feat(ORM): related name 7.6

--- Django/Airbnb/direct_messages/models.py
+++ Django/Airbnb/direct_messages/models.py
@@ -8,7 +8,9 @@ class ChattingRoom(CommonModel):
 
     users = models.ManyToManyField(
         "users.User",
+        related_name="chatting_rooms",
     )
+
 
     def __str__(self):
         return "Chatting Room"
@@ -24,10 +26,12 @@ class Message(CommonModel):
         null=True,
         blank=True,
         on_delete=models.SET_NULL,
+        related_name="messages",
     )
     room = models.ForeignKey(
         "direct_messages.ChattingRoom",
         on_delete=models.CASCADE,
+        related_name="messages",
     )
 
     def __str__(self):
