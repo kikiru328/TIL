8aad92163b249eda112dd0de9859293891da3888
kikiru328 <kikiru328@gmail.com>
2025-04-14 14:09:24 +0900

    feat(DRF): API, Serializers Method Field 11.11

--- Django/Airbnb/rooms/serializers.py
+++ Django/Airbnb/rooms/serializers.py
@@ -1,16 +1,16 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from .models import Amenity, Room
 from users.serializers import TinyUserSerializer
 from categories.serializers import CategorySerializer
 
-class AmenitySerializer(ModelSerializer):
+class AmenitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Amenity
         fields = (
             "name",
             "description",
         )
-class RoomDetailSerializer(ModelSerializer):
+class RoomDetailSerializer(serializers.ModelSerializer):
 
     owner = TinyUserSerializer(read_only=True)
     amenities = AmenitySerializer(
@@ -20,8 +20,16 @@ class RoomDetailSerializer(ModelSerializer):
     category = CategorySerializer(
         read_only=True,
     )
+    rating = serializers.SerializerMethodField()
 
-class RoomListSerializer(ModelSerializer):
+    class Meta:
+        model = Room
+        fields = "__all__"
+
+    def get_rating(self, room):
+        return room.rating()
+
+class RoomListSerializer(serializers.ModelSerializer)
     class Meta:
         model = Room
         fields = (
@@ -30,4 +38,7 @@ class RoomListSerializer(ModelSerializer):
             "country",
             "city",
             "price",
+            "rating",
         )
+    def get_rating(self, room):
+        return room.rating()
