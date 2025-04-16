c4b42de540b511e3f2c7324c92bb935e5bdbe5b3
kikiru328 <kikiru328@gmail.com>
2025-04-16 14:53:42 +0900

    test(authentication): authenticate login test 16.5

--- Django/Airbnb/rooms/tests.py
+++ Django/Airbnb/rooms/tests.py
@@ -1,4 +1,6 @@
 from rest_framework.test import APITestCase
+
+from users.models import User
 from . import models
 
 class TestAmenities(APITestCase):
@@ -69,6 +71,10 @@ class TestAmenity(APITestCase):
             name=self.NAME,
             description=self.DESC
         )
+    def test_amenity_not_found(self):
+        response = self.client.get("/api/v1/rooms/amenities/2")
+
+        self.assertEqual(response.status_code, 404)
 
     def test_get_amenity(self):
         response = self.client.get("/api/v1/rooms/amenities/2")
@@ -126,3 +132,23 @@ class TestAmenity(APITestCase):
     def test_delete_amenity(self):
         response = self.client.delete("/api/v1/rooms/amenities/1")
         self.assertEqual(response.status_code, 204)
+
+class TestRooms(APITestCase):
+
+    def setUp(self):
+        user = User.objects.create(
+            username="test",
+        )
+        user.set_password("123")
+        user.save()
+        self.user = user
+
+    def test_create_room(self):
+
+        response = self.client.post(
+            path="/api/v1/rooms/"
+        )
+
+        self.assertEqual(response.status_code, 403)
+
+        self.client.force_login(self.user,)
