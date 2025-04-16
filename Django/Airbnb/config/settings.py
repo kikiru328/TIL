d981a83bd5213a92731d2d7ae22acb27de0676f1
kikiru328 <kikiru328@gmail.com>
2025-04-16 09:31:22 +0900

    chore(environ): Environment File with django-environ 15.6

--- Django/Airbnb/config/settings.py
+++ Django/Airbnb/config/settings.py
@@ -11,17 +11,21 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import environ
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r5-5$njlg5y^2qe%9abr01%!o+a2%^qt9ag9jdl5ns6q5zs7qe'
-
+SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
